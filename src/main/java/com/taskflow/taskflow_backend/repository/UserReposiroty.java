0694015c203c7556a586012bf4cd305a714f1389
archis1405 <archismandas1405@gmail.com>
2026-03-08 01:06:47 +0530

    Added the user repository

---
+++ src/main/java/com/taskflow/taskflow_backend/repository/UserReposiroty.java
@@ -0,0 +1,25 @@
+package com.taskflow.taskflow_backend.repository;
+
+import com.taskflow.taskflow_backend.entity.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+import java.util.Optional;
+
+@Repository
+public interface UserReposiroty extends JpaRepository<User, Long> {
+    Optional<User> findByEmail(String email);
+
+    Optional<User> findByUsername(String username);
+
+    boolean existsByEmail(String email);
+
+    boolean existsByUsername(String username);
+
+    @Query("SELECT u FROM User u WHERE u.username LIKE %:query% OR u.email LIKE %:query% OR u.fullName LIKE %:query%")
+    List<User> searchUsers(@Param("query") String query);
+
+}
